4514bcb9c8ec90a144b2655bd34fe9a677363b99
Ronit Kumar Sarkar <68046238+Codee0101@users.noreply.github.com>
2020-10-01 12:17:15 +0530

    Update main.py
    Removed some redundant commands

--- main.py
+++ main.py
@@ -5,44 +5,9 @@ import os
 import pandas as pd
 import time
 from utils import append_df_to_excel
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
-
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
 
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
-
-# # Load a sample picture and learn how to recognize it.
-# obama_image = face_recognition.load_image_file("obama.jpg")
-# obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-
-# # Load a second sample picture and learn how to recognize it.
-# biden_image = face_recognition.load_image_file("biden.jpg")
-# biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
-
-# # Load a second sample picture and learn how to recognize it.
-# ronit_image = face_recognition.load_image_file("ronit.png")
-# ronit_face_encoding = face_recognition.face_encodings(ronit_image)[0]
-# # Create arrays of known face encodings and their names
-# panda_image = face_recognition.load_image_file("panda.png")
-# panda_face_encoding = face_recognition.face_encodings(panda_image)[0]
-# known_face_encodings = [
-#     obama_face_encoding,
-#     biden_face_encoding,
-#     ronit_face_encoding,
-#     panda_face_encoding
-# ]
-# known_face_roll_no = [
-#     "Barack Obama",
-#     "Joe Biden",
-#     "Ronit Sarkar",
-#     "Ashutosh Panda"
-# ]
 
 # Initialize some variables
 known_face_encodings = []
@@ -78,6 +43,7 @@ while True:
     ret, frame = video_capture.read()
 
     # Resize frame of video to 1/4 size for faster face recognition processing
+    #Resizing removed to increase the accuracy of the model
     small_frame = cv2.resize(frame, (0, 0), fx=1, fy=1)
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
@@ -130,6 +96,7 @@ while True:
         # right *= 2
         # bottom *= 2
         # left *= 2
+        # All scalings removed since images are processed at full resolution
 
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
